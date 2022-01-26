144c9686267c80abf33d29c57363f471674aa3b8
Darshana <keerthi.mac@gmail.com>
2022-01-26 21:45:11 +0530

    bank details added to database

---
+++ data/bank_info/bank_info.sql
@@ -0,0 +1,26 @@
+USE materials;
+--@block
+CREATE TABLE banks(
+    bank_code INT NOT NULL,
+    bank_name VARCHAR(250),
+    PRIMARY KEY(bank_code)
+);
+--@block
+CREATE TABLE branches(
+    branch_id INT AUTO_INCREMENT,
+    bank_code INT NOT NULL,
+    branch_code INT,
+    branch_location VARCHAR(250),
+    PRIMARY key(branch_id),
+    FOREIGN KEY (bank_code) REFERENCES banks(bank_code)
+);
+--@block
+SELECT bank_name,
+    branch_code,
+    branch_location
+FROM banks AS bk
+    LEFT JOIN branches AS br ON bk.bank_code = br.bank_code;
+--@block
+DROP TABLE branches;
+--@block
+DROP TABLE banks;
